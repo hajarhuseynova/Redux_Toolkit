cbc3995a46ca6bde181e84c357e4f6759e847c79
hajarhuseynova <hajarih@code.edu.az>
2025-03-17 20:24:13 +0400

    update

--- src/App.js
+++ src/App.js
@@ -2,17 +2,29 @@ import Navbar from './components/Navbar'
 import CartContainer from './components/CartContainer'
 import { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { calculateTotals } from './features/cart/cartSlice'
+import { calculateTotals, getCartItems } from './features/cart/cartSlice'
 import Modal from './components/Modal'
 
 function App() {
-  const { cartItems } = useSelector((state) => state.cart)
+  const { cartItems, isLoading } = useSelector((state) => state.cart)
   const { isOpen } = useSelector((state) => state.modal)
 
   const dispatch = useDispatch()
   useEffect(() => {
     dispatch(calculateTotals())
   }, [cartItems])
+
+  useEffect(() => {
+    dispatch(getCartItems())
+  }, [])
+
+  if (isLoading) {
+    return (
+      <div className="loading">
+        <h3>Loading....</h3>
+      </div>
+    )
+  }
 
   return (
     <main>
--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,5 +1,14 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import cartItems from '../../cartItems'
+
+const url = 'https://www.course-api.com/react-useReducer-cart-project'
+
+export const getCartItems = createAsyncThunk('cart/getCartItems', () => {
+  return fetch(url)
+    .then((resp) => resp.json())
+    .catch((err) => console.log(err))
+})
+
 const initialState = {
   cartItems: cartItems,
   amount: 0,
@@ -35,6 +44,19 @@ const cartSlice = createSlice({
       })
       state.amount = amount
       state.total = total
+    },
+  },
+  extraReducers: {
+    [getCartItems.pending]: (state) => {
+      state.isLoading = true
+    },
+    [getCartItems.fulfilled]: (state, action) => {
+      console.log(action)
+      state.isLoading = false
+      state.cartItems = action.payload
+    },
+    [getCartItems.rejected]: (state) => {
+      state.isLoading = false
     },
   },
 })
